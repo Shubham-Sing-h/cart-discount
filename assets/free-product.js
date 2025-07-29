c80e7e5e4a7960ca4956a9c9b2b9a3e03b88239c
Shubham Singh <shubham.singh@ens.enterprises>
2025-07-29 12:33:33 +0530

    First commit

---
+++ assets/free-product.js
@@ -0,0 +1,19 @@
+      // document.querySelectorAll('.quantity__button[name="minus"]').forEach(button => {
+      // button.addEventListener('click', () => {
+      //   alert(1)
+      //   setTimeout(() => {
+      //       this.checkCartForThresholdMinus();
+      //   }, 500);
+      // });
+      // });
+
+      // // setTimeout(() => {
+      // //   document.querySelectorAll('.quantity__button[name="plus"]').forEach(button => {
+      // //     alert(5566);
+      // //     button.addEventListener('click', () => {
+      // //       setTimeout(() => {
+      // //           this.checkCartForThreshold();
+      // //       }, 500);
+      // //     });
+      // //   });
+      // // }, 700);
